9d42ad63bc6ae81a7c937392d15e35cf2784b220
zhanglingyu <zhanglingyu@58.com>
2021-03-02 15:31:23 +0800

    提交

--- TestSwift/TestSwift/21_内存管理.swift
+++ TestSwift/TestSwift/21_内存管理.swift
@@ -55,11 +55,165 @@ class Person21 {
     }
 }
 
+/*
+ @escaping
+ 1、非逃逸闭包、逃逸闭包，一般都是当做参数传递给函数
+ 2、非逃逸闭包：闭包调用发生在函数结束前，闭包调用在函数作用域内
+ 3、逃逸闭包：闭包有可能在函数结束后调用，闭包调用逃离了函数的作用域，需要通过@escaping声明
+ */
+typealias Fn21 = () -> ()
+//fn是非逃逸闭包
+func test21_1(_ fn: Fn21) {
+    fn()
+}
+//fn是逃逸闭包
+var gFn21: Fn21?
+func test21_2(_ fn: @escaping Fn21) {
+    gFn21=fn
+}
+//fn是逃逸闭包
+func test21_3(_ fn: @escaping Fn21) {
+    DispatchQueue.global().async {
+        fn()
+    }
+}
+class Person21_1 {
+    var fn: Fn21
+    // fn是逃逸闭包
+    init(fn: @escaping Fn21) {
+        self.fn = fn
+    }
+    func run() {
+        // DispatchQueue.global().async 也是一个逃逸闭包
+        // 它用到了实例成员（属性、方法），编译器会强制要求明确写出self
+        DispatchQueue.global().async {
+            self.fn()
+        }
+    }
+}
+
+/*
+ 逃逸闭包的注意点
+ 逃逸闭包不可以捕获inout参数
+ */
+func other21_1(_ fn: Fn21){
+    fn()
+}
+func other21_2(_ fn: @escaping Fn21){
+    fn()
+}
+func test21_4(value: inout Int) {
+    other21_1 {
+        value += 1
+    }
+    //error：逃逸闭包不能捕获inout参数
+//    other21_2 {
+//        value += 1
+//    }
+}
+
+/*
+ 内存访问冲突
+
+ 内存访问冲突会在两个访问满足下列条件时发生：
+ 1、至少一个是写入操作
+ 2、它们访问的是同一块内存
+ 3、它们的访问时间重叠（比如在同一个函数内）
+
+ 如果下面的条件可以满足，就说明重叠访问结构体的属性时安全的
+ 1、你只访问实例存储属性，不是计算属性或者类属性
+ 2、结构体是局部变量而非全局变量
+ 3、结构体要么没有被闭包捕获要么只被非逃逸闭包捕获
+ */
+func balance(_ x: inout Int, _ y: inout Int) {
+    let sum = x + y
+    x = sum / 2
+    y = sum - x
+}
+struct Player {
+    var name: String
+    var health: Int
+    var energy: Int
+    mutating func shareHealth(with teammate: inout Player) {
+        balance(&teammate.health, &health)
+    }
+}
+
+/*
+ 指针
+ Swift中也有专门的指针类型，这些都被定性为"Unsafe"（不安全的）
+ 1、UnsafePointer<Pointee> 类似于 const Pointee *
+ 2、UnsafeMutablePointer<Pointee> 类似于 Pointee *
+ 3、UnsafeRawPointer 类似于 const void *
+ 4、UnsafeMutableRawPointer 类似于 void *
+ */
+var age = 10
+func test21_11(_ ptr:UnsafeMutablePointer<Int>) {
+    ptr.pointee += 10
+}
+func test21_22(_ ptr:UnsafePointer<Int>) {
+    print(ptr.pointee)
+}
+func test21_33(_ ptr:UnsafeMutableRawPointer) {
+    ptr.storeBytes(of: 20, as: Int.self)
+}
+func test21_44(_ ptr:UnsafeRawPointer) {
+    print(ptr.load(as: Int.self))
+}
+
+
 func testMemoryManagement() {
 
-    let p = Person21()
-    p.fn = {
-        [weak p] in p?.run()
-    }
+    //获得指向堆空间实例的指针
+    class Person {}
+    var person = Person()
+    var ptr = withUnsafePointer(to: &person) { UnsafeRawPointer($0) }
+    var heapPtr = UnsafeRawPointer(bitPattern: ptr.load(as: UInt.self))
+    print(heapPtr!)
+
+    //获得指向某个变量的指针
+//    var age = 11
+//    var ptr1 = withUnsafeMutablePointer(to: &age) { $0 }
+//    var ptr2 = withUnsafePointer(to: &age) { $0 }
+//    ptr1.pointee = 22
+//    print(ptr2.pointee)
+//    print(age)
+
+    //指针的应用示例
+//    let arr = NSArray(objects: 11, 22, 33, 44)
+//    arr.enumerateObjects { (obj, idx, stop) in
+//        print(idx, obj)
+//        if idx == 2 {
+//            stop.pointee = true
+//        }
+//    }
+//    for (idx, obj) in arr.enumerated() {
+//        print(idx, obj)
+//        if idx == 2 {
+//            break
+//        }
+//    }
+
+//    test21_11(&age)
+//    test21_22(&age) //20
+//    print(age)  //20
+//    test21_33(&age)
+//    test21_44(&age) //20
+//    print(age)  //20
+
+//    var tulpe = (health: 10, energy: 20)
+//    balance(&tulpe.health, &tulpe.energy)
+//    var holly = Player(name: "Holly", health: 10, energy: 10)
+//    balance(&holly.health, &holly.energy)
+//
+//    var oscar = Player(name: "Oscar", health: 10, energy: 10)
+//    var maria = Player(name: "Maria", health: 5, energy: 10)
+//    oscar.shareHealth(with: &maria)  //OK
+    //oscar.shareHealth(with: &oscar)  //Error
+
+//    let p = Person21()
+//    p.fn = {
+//        [weak p] in p?.run()
+//    }
 
 }
